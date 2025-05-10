faf3c6d39076e9e305ad49848a4bfcb1bd89684b
Luc <luc001@e.ntu.edu.sg>
2025-05-10 20:45:04 +0000

    Cleaned up launch files

--- launch/leader_bringup.launch.py
+++ launch/leader_bringup.launch.py
@@ -1,67 +1,35 @@
 import os
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
+from launch import LaunchDescription, Action
+from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction, OpaqueFunction, GroupAction
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
 
+launch_args = [
+    DeclareLaunchArgument('tree_name', default_value='TestLeader1Sam', description='Name of the behavior tree to load'),
+    DeclareLaunchArgument('ns', description='Namespace for the agent.'),
+]
+
 def generate_launch_description():
 
-    # config = os.path.join(get_package_share_directory('formation_controller'), 'config', 'overall_params.yaml')
+    config = os.path.join(get_package_share_directory('formation_controller'), 'config', 'overall_params.yaml')
 
-    tree_name_arg = DeclareLaunchArgument(
-        'tree_name',
-        default_value='Test',
-        description='Name of the behavior tree to load'
-    )
-
-    # # Subscribe to gps and heading topic of supreme leader. Broadcast world (utm) -> map, no translation, only rotation
-    # # Publish origin gps to /NS/origin_gps so that each agent can calculate its current local position, FLU frame
-    # origin_pub = Node(
-    #     name='origin_pub',
-    #     executable='origin_pub',
-    #     package='formation_controller', parameters=[config]
-    # )
-
-    # # Listen to gps and heading of this agent. Calculate the local position relative to origin_gps
-    # # And then broadcast the transform world (utm) -> agent
-    # # Only used for agents on the surface with access to gps (aka leaders)
-    # gps_heading_to_tf = Node(
-    #     name='gps_heading_to_tf',
-    #     executable='gps_heading_to_tf',
-    #     package='formation_controller', parameters=[config],
-    # )
-
-    origin_pub = Node(
-        name='origin_pub',
-        executable='origin_pub',
-        package='arduagent',
-        output='screen',
-        parameters=[{
-            'input_topic': 'core/gps',
-            'output_topic': 'origin_gps'
-        }]
-    )
-
-    mqtt_client = IncludeLaunchDescription(
-        FrontendLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('mqtt_client'), 'launch', 'standalone.launch.ros2.xml')
-        ), launch_arguments={'params_file': os.path.join(get_package_share_directory('arduagent'), 'config', 'mqtt_params_leader.yaml')}.items()
-    )
+    ns = LaunchConfiguration('ns')
+    tree_name = LaunchConfiguration('tree_name')
 
     leader_bt = Node(
         name='leader_bt',
         executable='bt_planner',
         package='tuper_btcpp',
         output='screen',
-        parameters=[{
+        namespace=ns,
+        parameters=[config, {
             'xml_directory': os.path.join(get_package_share_directory('tuper_btcpp'), 'behavior_trees'),
-            'tree_name': LaunchConfiguration('tree_name'),  # Active tree_name
-            'loop_rate': 20
+            'tree_name': tree_name,  # Active tree_name
+            'loop_rate': 20.0,
         }]
     )
-    leader_bt = TimerAction(period=7.0, actions=[leader_bt])
 
     # Relay nodes for Unity with SAM
     relay_nodes = IncludeLaunchDescription(
@@ -69,12 +37,10 @@ def generate_launch_description():
             os.path.join(get_package_share_directory('sam_thruster_relay'), 'launch', 'relay_nodes.launch.py')
         )
     )
+    relay_nodes = GroupAction([PushRosNamespace(ns), relay_nodes])
 
     return LaunchDescription([
-        tree_name_arg,
-        origin_pub,
-        mqtt_client,
-        # gps_heading_to_tf,
-        relay_nodes,
+        *launch_args,
         leader_bt,
+        relay_nodes,
     ])
--- launch/tuper_bringup.launch.py
+++ launch/tuper_bringup.launch.py
@@ -1,57 +1,77 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction, ExecuteProcess, GroupAction
-from launch.launch_description_sources import PythonLaunchDescriptionSource, FrontendLaunchDescriptionSource
-from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    foxglove = IncludeLaunchDescription(
-        FrontendLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('foxglove_bridge'), 'launch', 'foxglove_bridge_launch.xml')
-        )
+    # Contains use_sim_time param
+    config = os.path.join(get_package_share_directory('formation_controller'), 'config', 'overall_params.yaml')
+
+    # Subscribe to leader1 GPS and publish the first GPS received as the origin for all GPS computations
+    origin_pub = Node(
+        name='origin_pub',
+        executable='origin_pub',
+        package='arduagent',
+        namespace='leader1',
+        output='screen',
+        parameters=[config, {
+            'input_topic': 'core/gps',
+            'output_topic': '/origin_gps'
+        }]
     )
 
-    agent0_bringup = IncludeLaunchDescription(
+    # Leaders
+    leader1_bringup = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('tuper_btcpp'), 'launch', 'leader_bringup.launch.py')
         ), launch_arguments={
-            'tree_name': 'Test',
+            'tree_name': 'TestLeader1Sam',
+            'ns': 'leader1',
         }.items()
     )
-    agent0_bringup = TimerAction(period=1.0, actions=[PushRosNamespace('agent0'), agent0_bringup])
-
-    # agent1_bringup = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(get_package_share_directory('formation_controller'), 'launch', 'agent_bringup.launch.py')
-    #     ), launch_arguments={
-    #         # 'namespace': 'agent1',
-    #         'use_gps': 'True',
-    #         'is_unity_sam': 'True',
-    #     }.items()
-    # )
-    # agent1_bringup = GroupAction(actions=[PushRosNamespace('agent1'), agent1_bringup])
-
-    record_bag = TimerAction(
-        period=0.5,  # Delay in seconds
-        actions=[
-            ExecuteProcess(
-                cmd=['ros2', 'bag', 'record', '-a'],  # Record all topics
-            )
-        ]
+    leader2_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('tuper_btcpp'), 'launch', 'leader_bringup.launch.py')
+        ), launch_arguments={
+            'tree_name': 'TestLeader2Sam',
+            'ns': 'leader2',
+        }.items()
     )
 
     # Temporary node
     map_gt = Node(
         package = "tf2_ros", 
         executable = "static_transform_publisher",
-        arguments = ["0", "0", "0", "-1.57", "0", "0", "world", "map_gt"],
+        arguments = ["0", "0", "0", "0", "0", "0", "world", "map_gt"],
+    )
+
+    ping_synchronizer = Node(
+        name='ping_synchronizer',
+        package='tuper_sim_utils',
+        executable='ping_synchronizer',
+        output='screen',
+        parameters=[config, {
+            'leader1_acoustic_topic': '/leader1/acoustic/write',
+            'leader2_acoustic_topic': '/leader2/acoustic/write',
+        }]
+    )
+
+    leader1_string_relay = Node(
+        name='leader1_string_relay',
+        executable='transform',
+        package='topic_tools',
+        output='screen',
+        arguments="/leader1/acoustic/write/string --field data /leader1/acoustic/write smarc_msgs/StringStamped smarc_msgs.msg.StringStamped(data=m) --import std_msgs smarc_msgs --wait-for-start".split()
     )
 
     return LaunchDescription([    
-        foxglove,
-        agent0_bringup,
-        map_gt,
-        # record_bag,
+        origin_pub,
+        leader1_string_relay,
+        TimerAction(period=0.0, actions=[map_gt]),
+        TimerAction(period=0.2, actions=[ping_synchronizer]),
+        TimerAction(period=1.0, actions=[leader1_bringup]),
+        TimerAction(period=1.5, actions=[leader2_bringup]),
     ])
